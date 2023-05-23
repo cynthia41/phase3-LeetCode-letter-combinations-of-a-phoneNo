965e4c4c13b2d6b8a353629cd6fd8b6712fe4044
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-05-24 00:44:33 +0300

    complete

--- combination.rb
+++ combination.rb
@@ -1,31 +1,39 @@
-class Letters
-    def letter_combinations(digits)
-        result = []
-        letters_hash = {"2": "abc",
-                        "3": "def",
-                        "4": "ghi",
-                        "5": "jkl",
-                        "6": "mno",
-                        "7": "pqrs",
-                        "8": "tuv",
-                        "9": "wxyz"}
-    end
-    def backtrack(i, currentstr)
-        if length(currentstr) == length(digits)
-            result.append(currentstr)
-            return
-            for c in letters_hash[digitd[i]]
-                backtrack(i +1, currentstr +c)
+require "pry"
+def letter_combinations(digits)
+  mapping = {
+    '2' => 'abc',
+    '3' => 'def',
+    '4' => 'ghi',
+    '5' => 'jkl',
+    '6' => 'mno',
+    '7' => 'pqrs',
+    '8' => 'tuv',
+    '9' => 'wxyz'
+  }
 
-                if digits
-                    backtrack(0, "")
-
-                    return result
-                end
-            end
-        end
-    end
+  result = []
+digits.chars.each do |digit|
+  if result.empty? #Checks if array is empry i.e. first iteration
+    letters = mapping[digit].chars #converts values with the digit's key into an array of characters
+    result = letters #Assigns the letters as the first combination
+  else
+    new_combinations = result.product(mapping[digit].chars) #Finds the cartesian product i.e all combinations
+    # binding.pry
+    result = new_combinations.map { |combination| combination.join } #Combines the array of arrays to one array
+    # binding.pry
+  end
 end
+
+  return result
+end
+
+pp letter_combinations("235")
+
+
+
+
+
+
 
 
         
